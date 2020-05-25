f28781623f6b16a76a200b6dd35ee7ee041a7599
Ilya Meleshko <ilya.meleshko@clarksons.com>
2020-05-25 12:26:25 +0100

    task with paramtrization and xfailed test is added

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,14 +7,14 @@ class ProductPage(BasePage):
         link = self.browser.find_element(*ProductPageLocators.ADD_BASKET_BUTTON)
         link.click()
 
-    def should_be_product_page_link(self):
-        self.should_be_product_page_url()
+    def should_be_product_page_link(self, compare_url):
+        self.should_be_product_page_url(compare_url)
         self.should_be_product_page_form()
         self.should_be_product_page_button()
 
-    def should_be_product_page_url(self):
+    def should_be_product_page_url(self, compare_url):
         get_url = self.browser.current_url
-        assert get_url == "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019", "Wrong link is provided to get product page"
+        assert get_url == compare_url, "Wrong link is provided to get product page"
 
     def should_be_product_page_form(self):
         assert self.is_element_present(*ProductPageLocators.ADD_BASKET_FORM), "Basket form is not presented"
@@ -27,11 +27,9 @@ class ProductPage(BasePage):
             self.get_element_from_tuple(*ProductPageLocators.NAME_OF_PRODUCT))
         name_of_product_added = self.get_text_from_element(self.get_element_from_tuple(
             *ProductPageLocators.NAME_OF_PRODUCT_ADDED))
-        print(name_of_product_added + '==' + name_of_product)
         assert name_of_product_added == name_of_product, "Product name is not equal to the product name in the basket"
 
     def is_product_price_equals_to_the_basket_amount(self):
         product_price = self.get_text_from_element(self.get_element_from_tuple(*ProductPageLocators.PRODUCT_PRICE))
         basket_amount = self.get_text_from_element(self.get_element_from_tuple(*ProductPageLocators.BASKET_AMOUNT))
-        print(product_price + '==' + basket_amount)
         assert product_price == basket_amount, "The price is not equal to the amount in the basket"
--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,32 @@
 from pages.product_page import ProductPage
+import pytest
+import time
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+# def test_guest_can_add_product_to_basket_no_promo(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)
+#     page.open()
+#     product_page = ProductPage(browser, browser.current_url)
+#     product_page.should_be_product_page_link()
+#     page.add_to_basket()
+#     page.solve_quiz_and_get_code()
+#     page.is_product_price_equals_to_the_basket_amount()
+#     page.is_product_name_equals_to_the_basket_product()
+
+
+@pytest.mark.parametrize('promo',
+                         [
+                             "0", "1", "2", "3", "4", "5", "6",
+                             pytest.param(7, marks=pytest.mark.xfail),
+                             "8", "9"
+                         ])
+def test_guest_can_add_product_to_basket_with_promo(browser, promo):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{}".format(promo)
     page = ProductPage(browser, link)
     page.open()
-    product_page = ProductPage(browser, browser.current_url)
-    product_page.should_be_product_page_link()
+    page.should_be_product_page_link(
+        "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=offer{}".format(promo))
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.is_product_price_equals_to_the_basket_amount()
